85d1adaebec4e677bde75cba9ce3dc0e5be0e99b
VictoriaS009 <143551765+VictoriaS009@users.noreply.github.com>
2023-12-02 07:41:30 +0100

    homework 08

--- 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Zoo/Zoo.jsx
+++ 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Zoo/Zoo.jsx
@@ -7,11 +7,61 @@ import Species from "../Species/Species";
 
 export default function Zoo() {
   /* Escribe acá tu código */
+  const [zoo, setZoo] = React.useState({
+    zooName:"",
+    animals:[],
+    species:[],
+    allAnimals:[]
+  })
+
+  function handleaInputChange(evento){
+    setZoo({
+      ...zoo,
+      zooName:evento.target.value
+    })
+  }
+
+
+  function handleSpecies(evento){
+    setZoo({
+      ...zoo,
+      animals:zoo.allAnimals.filter((animal)=> animal.
+      specie === evento.target.value)
+    })
+  }
+  function handleAllSpecies(){
+    setZoo({
+      ...zoo,
+      animal:zoo.allAnimals
+    })
+  }
+
+
+  React.useEffect(()=>{
+    fetch("http://localhost:3001/zoo")
+    .then((res) => res.json())
+    .then((data) => {
+
+     return setZoo({
+        ...zoo,
+        animals: data.animals,
+        species: data.species,
+        allAnimals: data.animals,
+    })
+    }
+    )
+    .catch((error) => console.log(error));
+  },[])
 
 
   return (
     <div>
-      <h1>-</h1>
+      <label> Zoo Name:</label>
+      <input type="text" value={zoo.zooName} onChange={handleaInputChange} />
+      <h1>{zoo.zooName}</h1>
+      <Species species={zoo.species} handleSpecies={handleSpecies}
+      handleAllSpecies={handleAllSpecies}/>
+      <Animals animals={zoo.animals}/>
     </div>
   );
 }
